3c5e1a4d41ee9153f82b4abc9326ace39242ef98
rvnlord <rvnlord@gmail.com>
2022-06-30 02:37:08 +0200

    Async FIleInfo Extensions

--- CommonLib/CommonLib/Source/Common/Extensions/FileInfoExtensions.cs
+++ CommonLib/CommonLib/Source/Common/Extensions/FileInfoExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Threading.Tasks;
 using CommonLib.Source.Common.Utils;
 
 namespace CommonLib.Source.Common.Extensions
@@ -10,5 +12,17 @@ namespace CommonLib.Source.Common.Extensions
         {
             fileInfo.MoveTo(PathUtils.Combine(PathSeparator.BSlash, fileInfo.Directory?.FullName ?? throw new NullReferenceException(), newName));
         }
+
+        public static async Task RenameAsync(this FileInfo fileInfo, string newName) => await Task.Run(() => fileInfo.Rename(newName));
+
+        public static async Task DeleteAsync(this FileInfo fileInfo) => await Task.Run(fileInfo.Delete);
+
+        public static void DeleteAll(this IEnumerable<FileInfo> fileInfos)
+        {
+            foreach (var fi in fileInfos)
+                fi.Delete();
+        }
+
+        public static async Task DeleteAllAsync(this IEnumerable<FileInfo> fileInfos) => await Task.Run(fileInfos.DeleteAll);
     }
 }
