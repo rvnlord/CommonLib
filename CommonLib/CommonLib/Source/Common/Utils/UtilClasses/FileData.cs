fdee18b5512da59ea1425dd15a48893bb7cc3b1a
rvnlord <rvnlord@gmail.com>
2022-10-19 23:14:17 +0200

    Adjust FIleData to work with Custom FileUpload Component

--- CommonLib/CommonLib/Source/Common/Utils/UtilClasses/FileData.cs
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/FileData.cs
@@ -5,16 +5,19 @@ namespace CommonLib.Source.Common.Utils.UtilClasses
 {
     public class FileData
     {
-        public FileSize? TotalSize { get; set; }
+        public long? TotalSizeInBytes { get; set; }
+        public FileSize TotalSize => new (TotalSizeInBytes ?? 0);
         public FileSize ChunkSize => new (Data.Count);
-        public string Path { get; set; }
+        public string Path => PathUtils.Combine(PathSeparator.BSlash, DirectoryPath, Name, ".", Extension);
         public List<byte> Data { get; set; }
         public long Position { get; set; }
-        public double Progress => (double)Position / (TotalSize?.SizeInBytes ?? ChunkSize.SizeInBytes) * 100;
-        public string Name => Path?.AfterLast(@"\");
-        public string Extension => Name?.AfterLastOrNull(".");
+        public double Progress => (double)Position / (TotalSizeInBytes ?? ChunkSize.SizeInBytes) * 100;
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string DirectoryPath { get; set; }
         public bool IsSelected { get; set; }
         public UploadStatus Status { get; set; }
+        public string NameExtensionAndSize => $"{Name}.{Extension} ({TotalSize})";
     }
 
     public enum UploadStatus
