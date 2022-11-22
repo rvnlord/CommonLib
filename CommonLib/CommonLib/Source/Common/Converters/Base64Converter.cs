ac179b48a82fdd9126a2b28f0b87d6e6c7c696a0
rvnlord <rvnlord@gmail.com>
2022-11-22 21:46:20 +0100

    More flexible base64 converters

--- CommonLib/CommonLib/Source/Common/Converters/Base64Converter.cs
+++ CommonLib/CommonLib/Source/Common/Converters/Base64Converter.cs
@@ -1,27 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace CommonLib.Source.Common.Converters
 {
     public static class Base64Converter
     {
-        public static byte[] Base64ToByteArray(this string str) => Convert.FromBase64String(str);
+        public static bool IsBase64(this string base64) => Convert.TryFromBase64String(base64.PadRight(base64.Length / 4 * 4 + (base64.Length % 4 == 0 ? 0 : 4), '='), new Span<byte>(new byte[base64.Length]), out _);
+        public static byte[] Base64ToByteArray(this string base64) => Convert.FromBase64String(base64);
         public static string ToBase64String(this byte[] arr) => Convert.ToBase64String(arr);
-        public static string ToBase64String(this List<byte> list) => Convert.ToBase64String(list.ToArray());
+        public static string ToBase64String(this IEnumerable<byte> list) => Convert.ToBase64String(list.ToArray());
         public static string UTF8ToBase64(this string utf8str) => utf8str.UTF8ToByteArray().ToBase64String();
         public static string Base64ToUTF8(this string base64) => base64.Base64ToByteArray().ToUTF8String();
 
-        public static byte[] Base64ToByteArrayOrDefault(this string str)
-        {
-            try
-            {
-                return Convert.FromBase64String(str);
-            }
-            catch (FormatException)
-            {
-                return Array.Empty<byte>();
-            }
-        }
+        public static byte[] Base64ToByteArrayOrDefault(this string base64) => base64.IsBase64() ? base64.Base64ToByteArray() : default;
+        public static byte[] Base64ToByteArrayOrNull(this string base64) => base64.IsBase64() ? base64.Base64ToByteArray() : null;
+
+        public static string HexToBase64(this string str) => str.HexToByteArray().ToBase64String();
 
         public static string ToBase64StringOrDefault(this byte[] arr)
         {
@@ -31,7 +26,7 @@ namespace CommonLib.Source.Common.Converters
             }
             catch (FormatException)
             {
-                return string.Empty;
+                return default;
             }
         }
 
