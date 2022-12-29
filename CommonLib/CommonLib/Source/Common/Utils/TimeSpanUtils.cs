2cee14fbdaf12b176dc4af461a35abf3eae7a951
rvnlord <rvnlord@gmail.com>
2022-12-29 03:47:45 +0100

    FromMonths()

---
+++ CommonLib/CommonLib/Source/Common/Utils/TimeSpanUtils.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CommonLib.Source.Common.Utils
+{
+    public static class TimeSpanUtils
+    {
+        public static TimeSpan FromApproximateYears(int years) => TimeSpan.FromDays(years * 365.2425);
+        public static TimeSpan FromApproximateMonths(int months) => TimeSpan.FromDays((double)months / 12 * 365.2425);
+    }
+}
