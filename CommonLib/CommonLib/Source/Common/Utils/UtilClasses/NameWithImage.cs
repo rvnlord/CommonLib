c2e14435eab7e5b0d5f19bd63780ce213214fd92
rvnlord <rvnlord@gmail.com>
2023-01-12 19:49:24 +0100

    NameWithImage

---
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/NameWithImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CommonLib.Source.Common.Utils.UtilClasses
+{
+    public class NameWithImage : IEquatable<NameWithImage>
+    {
+        public string Name { get; set; }
+        public FileData Image { get; set; }
+
+        public bool Equals(NameWithImage other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name, StringComparison.InvariantCultureIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((NameWithImage)obj);
+        }
+
+        public override int GetHashCode() => Name is not null ? StringComparer.InvariantCultureIgnoreCase.GetHashCode(Name) : 0;
+        public static bool operator ==(NameWithImage left, NameWithImage right) => Equals(left, right);
+        public static bool operator !=(NameWithImage left, NameWithImage right) => !Equals(left, right);
+
+        public override string ToString() => Name;
+    }
+}
