57e78edb1b310e6a9e94d2ad35ea219469db45f1
rvnlord <rvnlord@gmail.com>
2023-04-14 23:32:11 +0200

    Add `TryEnableProviderAsync` to IEthereumHostProviderExtensions in order to safely fall through Metamask password and timeout rejections

---
+++ CommonLib/CommonLib/Source/Common/Extensions/IEthereumHostProviderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommonLib.Source.Models;
+using Nethereum.JsonRpc.Client;
+using Nethereum.UI;
+
+namespace CommonLib.Source.Common.Extensions
+{
+    public static class IEthereumHostProviderExtensions
+    {
+        public static async Task<ApiResponse<string>> TryEnableProviderAsync(this IEthereumHostProvider ethHostProvider)
+        {
+            try
+            {
+                return new ApiResponse<string>(await ethHostProvider.EnableProviderAsync());
+            }
+            catch (Exception ex) when (ex is TaskCanceledException or ObjectDisposedException or RpcResponseException)
+            {
+                return new ApiResponse<string>(StatusCodeType.Status401Unauthorized, ex.Message, ex);
+            }
+        }
+    }
+}
