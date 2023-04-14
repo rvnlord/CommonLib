50cc59beff35ef4ae6eb3bd34cd7e2e82040a666
rvnlord <rvnlord@gmail.com>
2023-04-14 23:30:13 +0200

    Add `TrySendRequestAsync` to IEthereumMessageSignExtensions to safely fall through Metamask signature rejections

---
+++ CommonLib/CommonLib/Source/Common/Extensions/IEthereumMessageSignExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using CommonLib.Source.Models;
+using Nethereum.Hex.HexTypes;
+using Nethereum.JsonRpc.Client;
+using Nethereum.RPC.AccountSigning;
+
+namespace CommonLib.Source.Common.Extensions
+{
+    public static class IEthereumMessageSignExtensions
+    {
+        public static async Task<ApiResponse<string>> TrySendRequestAsync(this IEthereumMessageSign ethMessageSign, string message, object id = null)
+        {
+            try
+            {
+                return new ApiResponse<string>(await ethMessageSign.SendRequestAsync(new HexUTF8String(message), id));
+            }
+            catch (Exception ex) when (ex is TaskCanceledException or ObjectDisposedException or RpcResponseException)
+            {
+                if (ex.Message.ContainsIgnoreCase("Metamask Message Signature"))
+                    return new ApiResponse<string>(StatusCodeType.Status401Unauthorized, ex.Message, ex);
+                throw;
+            }
+        }
+    }
+}
