b109c9eff59fcad046359cfdcbdb7c2797f63a27
rvnlord <rvnlord@gmail.com>
2023-05-10 02:41:28 +0200

    Plug 'DbContextOptionsBuilder' into custom `DbContextFactory` for easier config

---
+++ CommonLib/CommonLib/Source/Common/Extensions/DbContextOptionsBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using CommonLib.Source.Common.Utils.UtilClasses;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommonLib.Source.Common.Extensions
+{
+    public static class DbContextOptionsBuilderExtensions
+    {
+        public static DbContextOptionsBuilder<TContext> SQLiteFromAppConfig<TContext>(this DbContextOptionsBuilder<TContext> b, string csConfigName, string migrationAssembly = null) where TContext : DbContext
+        {
+            return DbContextFactory.GetSQLiteDbContextOptionsBuilder(csConfigName, migrationAssembly, b);
+        }
+    }
+}
