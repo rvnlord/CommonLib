23aa402d5929fbcba87cf8215cf565475305eed4
rvnlord <rvnlord@gmail.com>
2023-05-13 21:19:48 +0200

    `AuthenticationSchemeVM` and respective json converter

---
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/JsonSerialization/JsonConverters/AuthenticationSchemeVMJsonConverter.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using CommonLib.Source.ViewModels.Account;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using JsonConverter = Newtonsoft.Json.JsonConverter;
+
+namespace CommonLib.Source.Common.Utils.UtilClasses.JsonSerialization.JsonConverters
+{
+    public class AuthenticationSchemeVMJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(FileData);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (writer is null)
+                throw new ArgumentNullException(nameof(writer));
+
+            var a = (AuthenticationSchemeVM) value;
+            var jA = new JObject
+            {
+                [nameof(AuthenticationSchemeVM.Name)] = a.Name,
+                [nameof(AuthenticationSchemeVM.DisplayName)] = a.DisplayName,
+                [nameof(AuthenticationSchemeVM.HandlerType)] = a.HandlerType?.ToString() 
+            };
+
+            jA.WriteTo(writer);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (reader == null)
+                throw new ArgumentNullException(nameof(reader));
+            if (JToken.Load(reader) is not JObject jA)
+                return null;
+            
+            return new AuthenticationSchemeVM
+            (
+                jA[nameof(AuthenticationSchemeVM.Name)]?.ToString(),
+                jA[nameof(AuthenticationSchemeVM.DisplayName)]?.ToString(),
+                Type.GetType(jA[nameof(AuthenticationSchemeVM.HandlerType)]?.ToString() ?? "", false) ?? (Type) Type.Missing
+            );
+        }
+    }
+}
---
+++ CommonLib/CommonLib/Source/ViewModels/Account/AuthenticationSchemeVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using CommonLib.Source.Common.Extensions.Collections;
+
+namespace CommonLib.Source.ViewModels.Account;
+
+public class AuthenticationSchemeVM
+{
+    public AuthenticationSchemeVM(string name, string displayName, Type handlerType)
+    {
+        ArgumentNullException.ThrowIfNull(name);
+        //ArgumentNullException.ThrowIfNull(handlerType); // it should be possible to assign or deserialize handlerType to null value when the type is not present in client project
+        //if (!handlerType.GetInterfaces().Select(i => i.Name).ContainsIgnoreCase("IAuthenticationHandler"))
+        //    throw new ArgumentException("handlerType must implement IAuthenticationHandler.");
+
+        Name = name;
+        HandlerType = handlerType;
+        DisplayName = displayName;
+    }
+    
+    public string Name { get; }
+    public string DisplayName { get; } 
+    public Type HandlerType { get; }
+}
