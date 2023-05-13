32c2e8cc029ece6d3b236a27a979e0bb41cad217
rvnlord <rvnlord@gmail.com>
2023-05-13 21:18:40 +0200

    Fallback through invalid types into null instead of Missing due to WASM constraints

--- CommonLib/CommonLib/Source/Common/Utils/UtilClasses/JsonSerialization/JsonConverters/TypeJsonConverter.cs
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/JsonSerialization/JsonConverters/TypeJsonConverter.cs
@@ -24,8 +24,8 @@ namespace CommonLib.Source.Common.Utils.UtilClasses.JsonSerialization.JsonConver
         {
             if (reader == null)
                 throw new ArgumentNullException(nameof(reader));
-
-            return Type.GetType(reader.Value?.ToString() ?? "", false) ?? Type.Missing; // do not throw if we are deserializing types that are not present in the project (like when we are getting Authentication Schemes for display from Server but CLient is wasm and doesn't support them)
+            
+            return Type.GetType(reader.Value?.ToString() ?? "", false); // do not throw if we are deserializing types that are not present in the project (like when we are getting Authentication Schemes for display from Server but Client is wasm and doesn't support them)
         }
     }
 }
