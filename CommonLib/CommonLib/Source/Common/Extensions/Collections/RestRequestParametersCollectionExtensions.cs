248ed0817f4d9684a17b0f1c396eb024761b3fb5
rvnlord <rvnlord@gmail.com>
2023-05-17 03:24:48 +0200

    `RemoveParameter()` for RestRequest

--- CommonLib/CommonLib/Source/Common/Extensions/Collections/RestRequestParametersCollectionExtensions.cs
+++ CommonLib/CommonLib/Source/Common/Extensions/Collections/RestRequestParametersCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using RestSharp;
 
@@ -6,6 +7,13 @@ namespace CommonLib.Source.Common.Extensions.Collections
 {
     public static class RestRequestParametersCollectionExtensions
     {
+        public static ParametersCollection RemoveParameter(this ParametersCollection pc, Parameter p)
+        {
+            var parameters = pc.GetField<List<Parameter>>("Parameters");
+            parameters.Remove(p);
+            return pc;
+        }
+
         public static ParametersCollection RemoveBy(this ParametersCollection pc, Func<Parameter, bool> selector)
         {
             foreach (var p in pc.Where(selector))
