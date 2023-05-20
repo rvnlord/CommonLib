aeb82197d56ac6ca8da43f8bf88743ee121bbdfa
rvnlord <rvnlord@gmail.com>
2023-05-20 22:34:48 +0200

    `ReleaseAsync()` and `ReleaseSafelyAsync()` for SemaphoreSlim

---
+++ CommonLib/CommonLib/Source/Common/Extensions/SemaphoreSlimExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace CommonLib.Source.Common.Extensions
+{
+    public static class SemaphoreSlimExtensions
+    {
+        public static async Task ReleaseSafelyAsync(this SemaphoreSlim semaphore)
+        {
+            if (semaphore.CurrentCount == 0)
+                await semaphore.ReleaseAsync();
+        }
+
+        public static async Task ReleaseAsync(this SemaphoreSlim semaphore)
+        {
+            await Task.FromResult(semaphore.Release());
+        }
+    }
+}
