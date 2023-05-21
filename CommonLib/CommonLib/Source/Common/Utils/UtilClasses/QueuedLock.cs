290a95aa8fe41fc57b2ed81a47202d8b9af7b738
rvnlord <rvnlord@gmail.com>
2023-05-21 03:35:02 +0200

    `QueuedLock` as a utility to enforce ordered execution for semaphores

---
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/QueuedLock.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+
+namespace CommonLib.Source.Common.Utils.UtilClasses
+{
+    public sealed class QueuedLock
+    {
+        private readonly object _innerLock;
+        private volatile int _ticketsCount;
+        private volatile int _ticketToRide = 1;
+        private readonly ThreadLocal<int> _reEnter = new();
+
+        public QueuedLock()
+        {
+            _innerLock = new object();
+        }
+
+        public void Enter()
+        {
+            _reEnter.Value++;
+            if (_reEnter.Value > 1)
+                return;
+            var myTicket = Interlocked.Increment(ref _ticketsCount);
+            Monitor.Enter(_innerLock);
+            while (true)
+            {
+
+                if (myTicket == _ticketToRide)
+                    return;
+
+                Monitor.Wait(_innerLock);
+            }
+        }
+
+        public void Exit()
+        {
+            if (_reEnter.Value > 0)
+                _reEnter.Value--;
+            if (_reEnter.Value > 0)
+                return;
+            Interlocked.Increment(ref _ticketToRide);
+            Monitor.PulseAll(_innerLock);
+            Monitor.Exit(_innerLock);
+        }
+    }
+}
