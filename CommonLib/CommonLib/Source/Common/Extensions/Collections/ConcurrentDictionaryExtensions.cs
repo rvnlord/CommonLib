b88b574109baad25231c51746698dee6dc174720
rvnlord <rvnlord@gmail.com>
2023-05-22 18:57:18 +0200

    `AddRange()` Extension for ConcurrentDictionary

--- CommonLib/CommonLib/Source/Common/Extensions/Collections/ConcurrentDictionaryExtensions.cs
+++ CommonLib/CommonLib/Source/Common/Extensions/Collections/ConcurrentDictionaryExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using CommonLib.Source.Common.Converters;
 
@@ -9,6 +10,19 @@ namespace CommonLib.Source.Common.Extensions.Collections
         public static IEnumerable<TValue> SafelyGetValues<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> cdict)
         {
             return cdict.SafelyToDictionary().Values;
+        }
+
+        public static ConcurrentDictionary<TKey, TValue> AddRange<TKey, TValue>(this ConcurrentDictionary<TKey, TValue> dict, IEnumerable<KeyValuePair<TKey, TValue>> newDict)
+        {
+            if (dict == null)
+                throw new ArgumentNullException(nameof(dict));
+            if (newDict == null)
+                throw new ArgumentNullException(nameof(newDict));
+
+            foreach (var (key, value) in newDict)
+                dict.TryAdd(key, value);
+
+            return dict;
         }
     }
 }
