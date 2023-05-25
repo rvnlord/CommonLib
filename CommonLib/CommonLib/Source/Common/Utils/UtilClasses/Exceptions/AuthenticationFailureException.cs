76e3b5358e909d77c057ef470c877ed07d80fdeb
rvnlord <rvnlord@gmail.com>
2023-05-25 22:25:19 +0200

    `AuthenticationFailureException`

---
+++ CommonLib/CommonLib/Source/Common/Utils/UtilClasses/Exceptions/AuthenticationFailureException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace CommonLib.Source.Common.Utils.UtilClasses.Exceptions;
+
+public class AuthenticationFailureException : Exception
+{
+    public AuthenticationFailureException(string message) : base(message)  { }
+    public AuthenticationFailureException(string message, Exception innerException) : base(message, innerException) { }
+}
