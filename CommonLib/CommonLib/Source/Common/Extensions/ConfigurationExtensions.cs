af903410a3f22c7a89fca454a0c2ca3dfe80d6e9
rvnlord <rvnlord@gmail.com>
2023-07-08 21:42:27 +0200

    Add `GetAuthenticationStorageMode()` to `ConfigurationExtensions`

---
+++ CommonLib/CommonLib/Source/Common/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,14 @@
+﻿using CommonLib.Source.Common.Converters;
+using CommonLib.Source.Common.Utils;
+using Microsoft.Extensions.Configuration;
+
+namespace CommonLib.Source.Common.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static AuthenticationStorageMode GetAuthenticationStorageMode(this IConfiguration configuration)
+        {
+            return configuration.GetSection("Authentication").GetValue<string>("StorageMode")?.ToEnum<AuthenticationStorageMode>() ?? AuthenticationStorageMode.NotSpeecified;
+        }  
+    }
+}
