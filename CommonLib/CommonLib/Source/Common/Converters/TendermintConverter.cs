a55a79db3e379711abafd6b1ea4ae0ad380ef99a
rvnlord <rvnlord@gmail.com>
2023-07-29 00:13:53 +0200

    `TendermintConverter` for tendermint compliant key and adress conversion

---
+++ CommonLib/CommonLib/Source/Common/Converters/TendermintConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using CommonLib.Source.Common.Utils;
+using NBitcoin.DataEncoders;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace CommonLib.Source.Common.Converters
+{
+    public static class TendermintConverter
+    {
+        public static byte[] TendermintPrivateKeyToTendermintPublicKey(this byte[] privKey)
+        {
+            if (privKey.Length != 64)
+                throw new FormatException("Tendermint private key should be 64 bytes long");
+            
+            AsymmetricKeyParameter privKeyparam = new Ed25519PrivateKeyParameters(privKey, 0);
+            var pubKey = ((Ed25519PrivateKeyParameters)privKeyparam).GeneratePublicKey().GetEncoded();
+            return pubKey;
+        }
+
+        public static byte[] TendermintPublicKeyToTendermintAddress(this byte[] pubKey)
+        {
+            if (pubKey.Length != 32)
+                throw new FormatException("Tendermint private key should be 32 bytes long");
+            
+            return pubKey.Sha256().Take(20).ToArray();
+        }
+        
+        public static byte[] TendermintPrivateKeyToTendermintAddress(this byte[] privKey) => privKey.TendermintPrivateKeyToTendermintPublicKey().TendermintPublicKeyToTendermintAddress();
+    }
+}
